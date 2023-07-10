348f7661f70c9b7b2d730754290f1984e0960735
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-07-10 15:48:28 +0200

    two-dimensional array

--- src/Arrays.java
+++ src/Arrays.java
@@ -9,5 +9,28 @@ public class Arrays {
         //Changing the length of an array
         int[] numbers = new int[7];
         numbers = new int[42]; // redeclaration
+
+        /*Processing a Two-Dimensional Array
+        Find the sum of the elements of a two-dimensional array.
+         */
+        int[][] array2D = { { 1, 2, 3, 4, 5 },
+                            { 5, 4, 3, 2, 1 },
+                            { 0, 2, 0, 4, 0 } };
+        int sum = 0;
+
+        for (int[]row : array2D) {
+            for (int element : row) {
+                sum += element;
+            }
+        }
+
+        for (int[] row : array2D) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("sum = " + sum);
+
     }
 }
