62ca48225eab0fbc5a095cfbe8bac0842963b9c0
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-08-04 21:22:30 +0200

    record

--- src/learn/decomposition/Item.java
+++ src/learn/decomposition/Item.java
@@ -1,2 +1,11 @@
-package learn.decomposition;public class Item {
+package learn.decomposition;
+
+public record Item(int itemId, String name) {
+    //The keyword record provides one more mechanism for creating a class with immutable instances.
+
+    /*
+    This record guarantees immutability of the values in the record fields and eliminates the need to create a constructor,
+    methods equals(Object), hashCode(), and toString(), which are generated automatically for the record.
+    Instead of getters, methods with a field name are generated—in this case, name() and id().
+     */
 }
--- src/learn/decomposition/ItemMain.java
+++ src/learn/decomposition/ItemMain.java
@@ -1,2 +1,16 @@
-package learn.decomposition;public class ItemMain {
+package learn.decomposition;
+
+public class ItemMain {
+    public static void main(String[] args) {
+        Item i1 = new Item(77, "A");
+        //getter
+        System.out.println(i1.itemId() + " " + i1.name());
+        //toString()
+        System.out.println(i1);
+        //equals()
+        Item i2 = new Item(77, "A");
+        System.out.println(i1.equals(i2));
+        //hashCode()
+        System.out.println(i1.hashCode() == i2.hashCode());
+    }
 }
