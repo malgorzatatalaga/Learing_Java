8c2eef0ed447d0ef0c8efcc955591a3014a4514a
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-08-05 17:24:05 +0200

    countdown task class

--- src/projects/interfaces/CountDownTask.java
+++ src/projects/interfaces/CountDownTask.java
@@ -1,13 +1,28 @@
 package projects.interfaces;
 
 public class CountDownTask implements Task {
+
+    private int value;
+
+    public CountDownTask(int value) {
+        this.value = Math.max(value, 0);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
     @Override
     public void execute() {
-
+        if (value > 0) {
+            value--;
+        }
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return value <= 0;
     }
 }
+
