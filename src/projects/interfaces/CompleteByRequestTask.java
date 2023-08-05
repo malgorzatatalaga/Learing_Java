26d23a5196b949c5b4a222896f354702ac1c97b9
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-08-05 17:23:26 +0200

    complete by request task class

--- src/projects/interfaces/CompleteByRequestTask.java
+++ src/projects/interfaces/CompleteByRequestTask.java
@@ -1,13 +1,27 @@
 package projects.interfaces;
 
 public class CompleteByRequestTask implements Task {
+
+    private boolean finished;
+    private boolean completed;
+
+    public CompleteByRequestTask() {
+    }
+
     @Override
     public void execute() {
-
+        if (completed) {
+            finished = true;
+        }
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return finished;
+    }
+
+    public void complete() {
+        completed = true;
     }
 }
+
