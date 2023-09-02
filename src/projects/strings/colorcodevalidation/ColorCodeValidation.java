e792e2c90f3fee5e9c24bdc806ee9b8f9770c3dc
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-09-02 15:04:51 +0200

    ColorCodeValidation

---
+++ src/projects/strings/colorcodevalidation/ColorCodeValidation.java
@@ -0,0 +1,15 @@
+package projects.strings.colorcodevalidation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorCodeValidation {
+    public static boolean validateColorCode(String color) {
+        if (color == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^(#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{8}))$");
+        Matcher matcher = pattern.matcher(color);
+        return matcher.matches();
+    }
+}
