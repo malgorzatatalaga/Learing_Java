2fc82fe3ddcd5cfc13f7c59522c4bbdec9e8391d
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-09-04 18:48:01 +0200

    local class

--- src/learn/nestedclasses/localclass/Ship.java
+++ src/learn/nestedclasses/localclass/Ship.java
@@ -1,2 +1,19 @@
-package learn.nestedclasses.localclass;public class Ship {
+package learn.nestedclasses.localclass;
+
+class Ship {
+    /**
+     * A local class has access to local variables of the enclosing block that are declared as final
+     * (for Java 7 and earlier) and effectively final (for Java 8 and later).
+     */
+    private int x = 10;
+    void work() {
+        //The variable y is of the effectively final type since its value does not change in the method
+        int y = 10;
+        class LocalClass {
+            public void test() {
+                x = 20;
+                System.out.println(x + " " + y);
+            }
+        }
+    }
 }
