693a7861301fc5d1ee1431c90132406ffc6f3f27
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-09-17 11:01:57 +0200

    cat class and its subclass kitten

---
+++ src/projects/generics/residents/cats/Kitten.java
@@ -0,0 +1,14 @@
+package projects.generics.residents.cats;
+
+public class Kitten extends Cat {
+
+    public Kitten(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Kitten " + name;
+    }
+}
+
