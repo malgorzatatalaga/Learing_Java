affb504c26fb9fb75b2d1f5a4b0db3092a4383c1
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-09-17 11:01:45 +0200

    dog class and its subclass puppy

---
+++ src/projects/generics/residents/dogs/Dog.java
@@ -0,0 +1,15 @@
+package projects.generics.residents.dogs;
+
+public class Dog {
+
+    protected String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog " + name;
+    }
+}
---
+++ src/projects/generics/residents/dogs/Puppy.java
@@ -0,0 +1,14 @@
+package projects.generics.residents.dogs;
+
+public class Puppy extends Dog {
+
+    public Puppy(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Puppy " + name;
+    }
+}
+
